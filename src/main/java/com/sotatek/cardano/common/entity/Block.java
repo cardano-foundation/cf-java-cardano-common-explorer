1a39cbdf24e0e19abea6d95a74df6bec504caae3
Sotatek-HaiPham2 <hai.pham2@sotatek.com>
2022-12-09 16:05:12 +0700

    remove slot leader is

--- src/main/java/com/sotatek/cardano/common/entity/Block.java
+++ src/main/java/com/sotatek/cardano/common/entity/Block.java
@@ -59,8 +59,8 @@ public class Block extends BaseEntity {
 //  @EqualsAndHashCode.Exclude
 //  private SlotLeader slotLeader;
 
-  @Column(name = "slot_leader_id", updatable = false, insertable = false)
-  private Long slotLeaderId;
+//  @Column(name = "slot_leader_id", updatable = false, insertable = false)
+//  private Long slotLeaderId;
 
   @Column(name = "size")
   private Integer size;
