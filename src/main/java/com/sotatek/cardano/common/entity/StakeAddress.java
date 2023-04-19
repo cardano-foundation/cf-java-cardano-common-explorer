e5edae67d08ace38d706e66a520d62f9344e32e4
Sotatek-TienDD <tien.do@sotatek.com>
2023-04-19 18:18:06 +0700

    feat: remove StakeAddress tx_id

--- src/main/java/com/sotatek/cardano/common/entity/StakeAddress.java
+++ src/main/java/com/sotatek/cardano/common/entity/StakeAddress.java
@@ -7,22 +7,15 @@ import java.math.BigInteger;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Digits;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.Hibernate;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "stake_address", uniqueConstraints = {
@@ -47,13 +40,6 @@ public class StakeAddress extends BaseEntity {
   @Column(name = "script_hash", length = 56)
   @Hash28Type
   private String scriptHash;
-
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @OnDelete(action = OnDeleteAction.CASCADE)
-  @JoinColumn(name = "tx_id", nullable = false,
-      foreignKey = @ForeignKey(name = "stake_address_tx_id_fkey"))
-  @EqualsAndHashCode.Exclude
-  private Tx tx;
 
   @Column(name = "balance", nullable = false, precision = 39)
   @Word128Type
