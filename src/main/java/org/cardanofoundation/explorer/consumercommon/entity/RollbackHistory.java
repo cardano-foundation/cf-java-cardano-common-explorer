82c0a3365a79d6b0c9b72ab5504d55b149ab0684
Sotatek-QuyenNguyen2 <quyen.nguyen2@sotatek.com>
2023-05-09 12:30:56 +0700

    feat: MET-918 redesign rollback history table for more precise information

--- src/main/java/org/cardanofoundation/explorer/consumercommon/entity/RollbackHistory.java
+++ src/main/java/org/cardanofoundation/explorer/consumercommon/entity/RollbackHistory.java
@@ -1,18 +1,21 @@
 package org.cardanofoundation.explorer.consumercommon.entity;
 
-import org.cardanofoundation.explorer.consumercommon.enumeration.BlocksDeletionStatus;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "rollback_history")
@@ -23,32 +26,18 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 public class RollbackHistory extends BaseEntity {
 
-  @Column(name = "block_no_start", nullable = false)
-  private Long blockNoStart;
+  @Column(name = "block_no", nullable = false)
+  private Long blockNo;
 
-  @Column(name = "block_slot_start", nullable = false)
-  private Long blockSlotStart;
+  @Column(name = "block_hash", nullable = false, length = 64)
+  private String blockHash;
 
-  @Column(name = "block_hash_start", nullable = false)
-  private String blockHashStart;
+  @Column(name = "slot_no", nullable = false)
+  private Long slotNo;
 
-  @Column(name = "block_no_end", nullable = false)
-  private Long blockNoEnd;
-
-  @Column(name = "block_slot_end", nullable = false)
-  private Long blockSlotEnd;
-
-  @Column(name = "block_hash_end", nullable = false)
-  private String blockHashEnd;
-
-  @Column(name = "reason")
-  private String reason;
-
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   @Column(name = "rollback_time", nullable = false)
   private Timestamp rollbackTime;
-
-  @Column(name = "blocks_deletion_status", nullable = false)
-  private BlocksDeletionStatus blocksDeletionStatus;
 
   @PrePersist
   private void prePersist() {
