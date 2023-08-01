db6b4b593e1fed546d9e5e97e26f40d83f84e796
trong.vu <trong.vu@sotatek.com>
2023-08-01 14:30:38 +0700

    feat: MET 1514: Implement sync-healthcheck endpoint

---
+++ src/main/java/org/cardanofoundation/explorer/consumercommon/constants/ExplorerConstant.java
@@ -0,0 +1,7 @@
+package org.cardanofoundation.explorer.consumercommon.constants;
+
+public class ExplorerConstant {
+    private ExplorerConstant() {
+    }
+    public static final String LAST_INSERTED_BLOCK_KEY = "LAST_INSERTED_BLOCK";
+}
