2722192d061474202fb08e07a81dbe4c7eaf480a
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-03-19 12:38:06 +0700

    feat: add new type of script purpose

--- src/main/java/org/cardanofoundation/explorer/common/entity/enumeration/ScriptPurposeType.java
+++ src/main/java/org/cardanofoundation/explorer/common/entity/enumeration/ScriptPurposeType.java
@@ -15,7 +15,9 @@ public enum ScriptPurposeType {
   SPEND("spend"),
   MINT("mint"),
   CERT("cert"),
-  REWARD("reward");
+  REWARD("reward"),
+  VOTE("vote"),
+  PROPOSE("propose");
 
   String value;
 
