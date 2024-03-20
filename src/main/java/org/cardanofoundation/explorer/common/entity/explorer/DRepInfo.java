12f9a316c04f363354278055fd0877ae9130880c
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-03-20 15:37:52 +0700

    feat: add DrepInfo entity

---
+++ src/main/java/org/cardanofoundation/explorer/common/entity/explorer/DRepInfo.java
@@ -0,0 +1,51 @@
+package org.cardanofoundation.explorer.common.entity.explorer;
+
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import org.cardanofoundation.explorer.common.entity.ledgersync.BaseEntity;
+
+@Entity
+@Table(name = "drep_info")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class DRepInfo extends BaseEntity {
+
+  @Column(name = "drep_hash", nullable = false, length = 56)
+  private String drepHash;
+
+  @Column(name = "drep_id", nullable = false)
+  private String drepId;
+
+  @Column(name = "anchor_url")
+  private String anchorUrl;
+
+  @Column(name = "anchor_hash")
+  private String anchorHash;
+
+  @Column(name = "delegators")
+  private Integer delegators;
+
+  @Column(name = "active_vote_stake")
+  private BigInteger activeVoteStake;
+
+  @Column(name = "live_stake")
+  private BigInteger liveStake;
+
+  @Column(name = "created_at")
+  private Long createdAt;
+}
