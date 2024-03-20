f5170a23df18a22535fb639516b00b933237984f
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-03-20 17:59:23 +0700

    feat: add entity relevant to governance action

---
+++ src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/enumeration/Vote.java
@@ -0,0 +1,13 @@
+package org.cardanofoundation.explorer.common.entity.ledgersync.enumeration;
+
+public enum Vote {
+  NO(0),
+  YES(1),
+  ABSTAIN(2);
+
+  private final int value;
+
+  private Vote(int value) {
+    this.value = value;
+  }
+}
