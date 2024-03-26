379076f12b83d9c260e01d4e9718d04c7188423e
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-03-26 17:09:59 +0700

    chore: fix enum Vote type

--- src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/enumeration/Vote.java
+++ src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/enumeration/Vote.java
@@ -1,14 +1,9 @@
 package org.cardanofoundation.explorer.common.entity.ledgersync.enumeration;
 
 public enum Vote {
-  NO(0),
-  YES(1),
-  ABSTAIN(2),
-  NONE(3);
-
-  private final int value;
-
-  private Vote(int value) {
-    this.value = value;
-  }
+  NO,
+  YES,
+  ABSTAIN,
+  NONE,
+  ANY,
 }
