93d63fdf94a8f87f64b175331873e5ba60cfdd24
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-04-02 12:35:03 +0700

    chore: fix enum Vote type

--- src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/enumeration/Vote.java
+++ src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/enumeration/Vote.java
@@ -1,9 +1,34 @@
 package org.cardanofoundation.explorer.common.entity.ledgersync.enumeration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@Getter
 public enum Vote {
-  NO,
-  YES,
-  ABSTAIN,
-  NONE,
-  ANY,
+  NO("NO"),
+  YES("YES"),
+  ABSTAIN("ABSTAIN"),
+  NONE("NONE"),
+  ANY("ANY");
+
+  private final String value;
+
+  private static final Map<String, Vote> voteMap = new HashMap<>();
+
+  static {
+    for (Vote vote : Vote.values()) {
+      voteMap.put(vote.value, vote);
+    }
+  }
+
+  public static Vote fromValue(String value) {
+    return voteMap.get(value);
+  }
 }
