c37044bd56aecef719757985b35b09aeaf07db7f
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-04-22 10:43:57 +0700

    feat: add common class report event

---
+++ src/main/java/org/cardanofoundation/explorer/common/model/ReportMessage.java
@@ -0,0 +1,17 @@
+package org.cardanofoundation.explorer.common.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.cardanofoundation.explorer.common.entity.explorer.ReportHistory;
+
+@Getter
+@Setter
+@Builder
+public class ReportMessage {
+
+  private ReportHistory reportHistory;
+  private long zoneOffset;
+  private String timePattern;
+}
