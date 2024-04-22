d272bf208a263b6677efc3b81927fe3fc884346f
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-04-22 13:15:37 +0700

    feat: add missing annotation

--- src/main/java/org/cardanofoundation/explorer/common/model/ReportMessage.java
+++ src/main/java/org/cardanofoundation/explorer/common/model/ReportMessage.java
@@ -1,13 +1,19 @@
 package org.cardanofoundation.explorer.common.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import org.cardanofoundation.explorer.common.entity.explorer.ReportHistory;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Builder
 public class ReportMessage {
 
