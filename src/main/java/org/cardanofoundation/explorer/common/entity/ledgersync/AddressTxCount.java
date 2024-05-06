c493e9eca09d33861fce881a9ae0cea7365b0bcd
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-05-06 11:10:54 +0700

    feat: add AddressTxCount entity

---
+++ src/main/java/org/cardanofoundation/explorer/common/entity/ledgersync/AddressTxCount.java
@@ -0,0 +1,28 @@
+package org.cardanofoundation.explorer.common.entity.ledgersync;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "address_tx_count")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class AddressTxCount {
+  @Id
+  @Column(name = "address", nullable = false)
+  String address;
+
+  @Column(name = "txCount", nullable = false)
+  Long txCount;
+}
